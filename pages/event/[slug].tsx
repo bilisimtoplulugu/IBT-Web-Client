2802b4227889d93cd076409fee0fadc10feaf54a
Berkay Doğukan Urhan <1800001773@STU.IKU.EDU.TR>
2020-05-22 23:13:58 +0300

    event detail page

--- pages/event/[slug].tsx
+++ pages/event/[slug].tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import PageTopSide from '../../components/PageTopSide'
 
 export default function EventDetail() {
     return (
         <Layout>
-            test
+              <PageTopSide title="Cem Ekinci - KolayBi" desc='Girişimcilik ve Globalleşme", "Covid-19 Girişimciliğe Etkileri ve Geleceği" ve "Girişimcilik ve Yatırım Süreçleri"nden bahsedeceği online etkinliğimize hepinizi bekliyoruz!'/>
+
         </Layout>
     )
 }
--- pages/event/index.tsx
+++ pages/event/index.tsx
@@ -83,7 +83,7 @@ export default function Event() {
 
         <Layout>
            <PageTopSide title="Etkinlikler" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-
+           
             <Container>
                 <Row>
                     <Col xs={12}>
