9e9066840f6c3d000d2373a3384791639113a86e
Berkay Doğukan Urhan <1800001773@STU.IKU.EDU.TR>
2020-05-28 18:13:32 +0300

    Playfair font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 const Wrapper = styled.div`
 
 background:#f8f8f8;
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500&display=swap');
 `;
 function Layout({ children }) {
     return (
